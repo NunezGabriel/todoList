b0970ffe1f6f72f0ec3d6aca103ee527e7cb97a3
Gabriel <emilio.nunez.arenas@gmail.com>
2023-05-22 21:51:51 -0500

    adding nice advance to todolist

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world");
+let todoName = document.querySelector('.tarea')
+let duedate = document.querySelector('.duedate')
+let botonSend = document.querySelector('.botonSend')
+let seccionTodos = document.querySelector('.todoListContainer')
+
+botonSend.addEventListener('click', send)
+
+function send(){
+    let todoListItem = document.createElement('P')
+    todoListItem.innerHTML = `${todoName.value} --- ends: ${duedate.value}`
+    seccionTodos.appendChild(todoListItem)
+}
