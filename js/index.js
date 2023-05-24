b6d82930a4aa3348b37e5fd222f365698c6af71e
Gabriel <emilio.nunez.arenas@gmail.com>
2023-05-24 12:56:18 -0500

    all teh js logic for the program -- Done

--- js/index.js
+++ js/index.js
@@ -2,11 +2,30 @@ let todoName = document.querySelector('.tarea')
 let duedate = document.querySelector('.duedate')
 let botonSend = document.querySelector('.botonSend')
 let seccionTodos = document.querySelector('.todoListContainer')
-
+let seccionDone = document.querySelector('.done')
 botonSend.addEventListener('click', send)
 
 function send(){
-    let todoListItem = document.createElement('P')
-    todoListItem.innerHTML = `${todoName.value} --- ends: ${duedate.value}`
+    let space = document.createElement('BR')
+    let copy = document.createElement('P')
+    let todoInput = document.createElement('INPUT')
+    todoInput.setAttribute('name', 'caja')
+    todoInput.setAttribute('type', 'checkbox')
+    let todoListItem = document.createElement('LABEL')
+    todoListItem.setAttribute('for', 'caja')
+    todoListItem.innerHTML = `${todoName.value} ||||| ends: ${duedate.value}`
+    copy.innerHTML = todoListItem.textContent
     seccionTodos.appendChild(todoListItem)
+    seccionTodos.appendChild(todoInput)
+    seccionTodos.appendChild(space)
+
+    todoInput.addEventListener('change', ()=>{
+        if (todoInput.checked) {
+            todoListItem.classList.add("marcado");
+            seccionDone.appendChild(copy)
+        } else {
+            todoListItem.classList.remove("marcado");
+            copy.remove()
+        }
+    })
 }
